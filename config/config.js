69b70afddd49cb7d58cc40894d372131412d0655
Chris Baird <cab3953@gmail.com>
2022-09-22 12:58:58 -0700

    Change database to mongoDB with Mongoose ODM

--- config/config.js
+++ config/config.js
@@ -1,19 +1,11 @@
-const Sequelize = require("sequelize")
+const mongoose = require("mongoose")
 
-require("dotenv").config()
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialmedia",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306,
-      }
-    )
-
-module.exports = sequelize
+module.exports = mongoose
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const exphbs = require("express-handlebars")
 const app = express()
 const PORT = process.env.PORT || 3001
 
-const sequelize = require("./config/config")
-const SequelizeStore = require("connect-session-sequelize")(session.Store)
+const mongoose = require("./config/config")
+const MongooseStore = require("mongoose-express-session")(session.Store)
 
 const sess = {
   secret: "Super secret secret",
@@ -19,8 +19,8 @@ const sess = {
   },
   resave: false,
   saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
+  store: new MongooseStore({
+    connection: mongoose,
   }),
 }
 
@@ -35,7 +35,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`)
-  sequelize.sync({ force: false })
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`)
+  })
 })
